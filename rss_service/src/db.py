d35b333ce526a560729b2985ce28aac329316ed1
Matvey Zaharchenko <matvey.ff@gmail.com>
2023-03-06 03:27:51 +0100

    Reformat code with black

--- rss_service/src/db.py
+++ rss_service/src/db.py
@@ -5,22 +5,25 @@ import logging
 from dataclasses import dataclass
 from typing import List, Optional
 
-DBNAME="rss_db"
-POOL_MIN_CONNECTIONS=1
-POOL_MAX_CONNECTIONS=10
+DBNAME = "rss_db"
+POOL_MIN_CONNECTIONS = 1
+POOL_MAX_CONNECTIONS = 10
 
-MAX_ENTRY_SIZE = 64*1024
+MAX_ENTRY_SIZE = 64 * 1024
 
 logging.basicConfig(level=logging.DEBUG)
+
 
 class UserNotFound(Exception):
     def __init__(self, username):
         self.username = username
         super().__init__(f"No user found: {username}")
 
+
 class UserAlreadyExists(Exception):
     def __init__(self, username):
         super().__init__(f"User already exists: {username}")
+
 
 class FeedNotFound(Exception):
     def __init__(self, feed_url):
@@ -44,12 +47,13 @@ class ConnectionManager(object):
 class DB:
     def __init__(self, host, port, user, password):
         self.pool = psycopg2.pool.SimpleConnectionPool(
-            POOL_MIN_CONNECTIONS, POOL_MAX_CONNECTIONS,
+            POOL_MIN_CONNECTIONS,
+            POOL_MAX_CONNECTIONS,
             host=host,
             port=port,
             database=DBNAME,
             user=user,
-            password=password
+            password=password,
         )
         with self.conn() as conn:
             with conn.cursor() as cursor:
@@ -63,7 +67,8 @@ class DB:
 
     def create_feeds_table(self, cursor):
         table = "Feeds"
-        cursor.execute(f'''
+        cursor.execute(
+            f"""
             CREATE TABLE IF NOT EXISTS {table} (
                 feed_id SERIAL PRIMARY KEY,
                 feed_url VARCHAR(255) UNIQUE,
@@ -71,15 +76,17 @@ class DB:
                 modified VARCHAR(255),
                 failed BOOLEAN DEFAULT false
             );
-        ''')
+        """
+        )
         if cursor.rowcount > 0:
             logging.info(f"Created table {table}")
         else:
             logging.info(f"Table {table} already exists")
 
     def create_feed_items_table(self, cursor):
-        table = 'FeedItems'
-        cursor.execute(f'''
+        table = "FeedItems"
+        cursor.execute(
+            f"""
             CREATE TABLE IF NOT EXISTS {table} (
                 item_id SERIAL PRIMARY KEY,
                 feed_id INTEGER,
@@ -87,28 +94,32 @@ class DB:
                 entry VARCHAR({MAX_ENTRY_SIZE}),
                 FOREIGN KEY (feed_id) REFERENCES Feeds (feed_id)
             );
-        ''')
+        """
+        )
         if cursor.rowcount > 0:
             logging.info(f"Created table {table}")
         else:
             logging.info(f"Table {table} already exists")
 
     def create_users_table(self, cursor):
-        table = 'Users'
-        cursor.execute(f'''
+        table = "Users"
+        cursor.execute(
+            f"""
             CREATE TABLE IF NOT EXISTS {table} (
                 user_id SERIAL PRIMARY KEY,
                 username VARCHAR(255) UNIQUE
             );
-        ''')
+        """
+        )
         if cursor.rowcount > 0:
             logging.info(f"Created table {table}")
         else:
             logging.info(f"Table {table} already exists")
 
     def create_user_feeds_table(self, cursor):
-        table = 'UserFeeds'
-        cursor.execute(f'''
+        table = "UserFeeds"
+        cursor.execute(
+            f"""
             CREATE TABLE IF NOT EXISTS {table} (
                 user_feed_id SERIAL PRIMARY KEY,
                 user_id INTEGER,
@@ -119,7 +130,8 @@ class DB:
                 FOREIGN KEY (last_read_item_id) REFERENCES FeedItems (item_id),
                 UNIQUE(user_id, feed_id)
             );
-        ''')
+        """
+        )
         if cursor.rowcount > 0:
             logging.info(f"Created table {table}")
         else:
@@ -129,34 +141,37 @@ class DB:
         with self.conn() as conn:
             with conn.cursor() as cursor:
                 cursor.execute(
-                    '''INSERT INTO Users (username) VALUES(%s)
+                    """INSERT INTO Users (username) VALUES(%s)
                        ON CONFLICT DO NOTHING
-                       RETURNING user_id''', (username,)
+                       RETURNING user_id""",
+                    (username,),
                 )
                 result = cursor.fetchone()
                 if not result:
                     raise UserAlreadyExists(username)
 
     def get_user_id(self, cursor, username: str):
-        cursor.execute('SELECT user_id FROM users WHERE username = %s', (username,))
+        cursor.execute("SELECT user_id FROM users WHERE username = %s", (username,))
         result = cursor.fetchone()
         if not result:
             raise UserNotFound(username)
         return result[0]
 
     def get_feed_id(self, cursor, feed_url):
-        cursor.execute('SELECT feed_id FROM feeds WHERE feed_url = %s', (feed_url,))
+        cursor.execute("SELECT feed_id FROM feeds WHERE feed_url = %s", (feed_url,))
         result = cursor.fetchone()
         if not result:
             raise FeedNotFound(feed_url)
         return result[0]
 
     def get_or_put_feed(self, cursor, url: str):
-        cursor.execute('SELECT feed_id FROM feeds WHERE feed_url = %s', (url,))
+        cursor.execute("SELECT feed_id FROM feeds WHERE feed_url = %s", (url,))
         result = cursor.fetchone()
         if result:
             return result[0]
-        cursor.execute('INSERT INTO feeds (feed_url) VALUES(%s) RETURNING feed_id', (url,))
+        cursor.execute(
+            "INSERT INTO feeds (feed_url) VALUES(%s) RETURNING feed_id", (url,)
+        )
         return cursor.fetchone()[0]
 
     def follow_feed(self, username: str, url: str):
@@ -165,34 +180,38 @@ class DB:
                 user_id = self.get_user_id(cursor, username)
                 feed_id = self.get_or_put_feed(cursor, url)
                 cursor.execute(
-                    '''INSERT INTO UserFeeds (user_id, feed_id) VALUES (%s, %s)
+                    """INSERT INTO UserFeeds (user_id, feed_id) VALUES (%s, %s)
                     ON CONFLICT DO NOTHING
-                    RETURNING user_feed_id''',
-                    (user_id, feed_id)
+                    RETURNING user_feed_id""",
+                    (user_id, feed_id),
                 )
                 return cursor.fetchone() is not None
 
     def unfollow_feed(self, username: str, feed_url: str):
-        '''
+        """
         After this call the feed is no longer followed.
         Return True if it existed beforehand, False otherwise
-        '''
+        """
         with self.conn() as conn:
             with conn.cursor() as cursor:
                 user_id = self.get_user_id(cursor, username)
                 feed_id = self.get_feed_id(cursor, feed_url)
-                cursor.execute("DELETE FROM UserFeeds WHERE user_id = %s AND feed_id = %s", (user_id, feed_id))
+                cursor.execute(
+                    "DELETE FROM UserFeeds WHERE user_id = %s AND feed_id = %s",
+                    (user_id, feed_id),
+                )
                 success = cursor.rowcount != 0
                 return success
-    
+
     def list_feeds(self, username: str):
         with self.conn() as conn:
             with conn.cursor() as cursor:
                 user_id = self.get_user_id(cursor, username)
                 cursor.execute(
-                    '''SELECT Feeds.feed_url FROM Feeds
+                    """SELECT Feeds.feed_url FROM Feeds
                        JOIN UserFeeds ON Feeds.feed_id = UserFeeds.feed_id
-                       WHERE UserFeeds.user_id = %s''', (user_id,)
+                       WHERE UserFeeds.user_id = %s""",
+                    (user_id,),
                 )
                 return [res[0] for res in cursor.fetchall()]
 
@@ -201,109 +220,134 @@ class DB:
             with conn.cursor() as cursor:
                 feed_id = self.get_feed_id(cursor, feed_url)
                 cursor.execute(
-                    '''SELECT etag, modified FROM Feeds WHERE feed_id = %s''',
-                    (feed_id,)
+                    """SELECT etag, modified FROM Feeds WHERE feed_id = %s""",
+                    (feed_id,),
                 )
                 result = cursor.fetchone()
                 if result is None:
                     return None
                 else:
-                    return {'etag': result[0], 'modified': result[1]}
+                    return {"etag": result[0], "modified": result[1]}
 
-    def put_updates(self, etag: Optional[str], modified: Optional[str],
-            feed_url: str, entries: List[str]):
-        entries = sorted(entries, key=lambda entry: entry['published'])
+    def put_updates(
+        self,
+        etag: Optional[str],
+        modified: Optional[str],
+        feed_url: str,
+        entries: List[str],
+    ):
+        entries = sorted(entries, key=lambda entry: entry["published"])
         with self.conn() as conn:
             with conn.cursor() as cursor:
                 feed_id = self.get_feed_id(cursor, feed_url)
-                query = 'INSERT INTO FeedItems (feed_id, published, entry) VALUES %s'
-                execute_values(cursor, query,
-                    [(feed_id, entry['published'], entry['content']) for entry in entries])
+                query = "INSERT INTO FeedItems (feed_id, published, entry) VALUES %s"
+                execute_values(
+                    cursor,
+                    query,
+                    [
+                        (feed_id, entry["published"], entry["content"])
+                        for entry in entries
+                    ],
+                )
                 if etag is not None or modified is not None:
-                    etag_query = 'etag = %s' if etag is not None else ''
-                    modified_query = 'modified = %s' if etag is not None else ''
+                    etag_query = "etag = %s" if etag is not None else ""
+                    modified_query = "modified = %s" if etag is not None else ""
                     values = (x for x in (etag, modified, feed_id) if x is not None)
-                    query = f'''UPDATE Feeds SET {etag_query} {modified_query} failed = false
-                                WHERE feed_id = %s'''
+                    query = f"""UPDATE Feeds SET {etag_query} {modified_query} failed = false
+                                WHERE feed_id = %s"""
                     cursor.execute(query, values)
 
     def set_failed(self, feed_url):
         with self.conn() as conn:
             with conn.cursor() as cursor:
-                cursor.execute('''UPDATE Feeds SET failed = true''')
-    
+                cursor.execute("""UPDATE Feeds SET failed = true""")
+
     def list_all_feeds(self):
         with self.conn() as conn:
             with conn.cursor() as cursor:
-                cursor.execute('SELECT feed_url FROM Feeds')
+                cursor.execute("SELECT feed_url FROM Feeds")
                 return [res[0] for res in cursor.fetchall()]
-    
+
     def get_feed_items(self, username: str, feed_url: str, unread_only: bool):
         with self.conn() as conn:
             with conn.cursor() as cursor:
                 user_id = self.get_user_id(cursor, username)
                 feed_id = self.get_feed_id(cursor, feed_url)
-                cursor.execute('''SELECT last_read_id FROM UserFeeds
+                cursor.execute(
+                    """SELECT last_read_id FROM UserFeeds
                                   WHERE user_id = %s AND feed_id = %s
-                                  ORDER BY published''',
-                                  (user_id, feed_id))
+                                  ORDER BY published""",
+                    (user_id, feed_id),
+                )
                 last_read = cursor.fetchone()
                 if last_read is None:
                     # Feed not found for particular user
                     raise FeedNotFound(feed_url)
                 last_read = last_read[0]
-                last_read_query = 'AND item_id > %s' if unread_only else ''
+                last_read_query = "AND item_id > %s" if unread_only else ""
                 if unread_only:
-                    last_read_query = 'AND item_id > %s'
+                    last_read_query = "AND item_id > %s"
                 else:
-                    last_read_query = ''
+                    last_read_query = ""
                     last_read = None
                 values = (x for x in (feed_id, last_read) if x is not None)
-                results = cursor.execute(f'''SELECT item_id, entry FROM FeedItems
-                    WHERE feed_id = %s {last_read_query}''',
-                    values).fetchall()
-                return [{'id': res[0], 'content': res[1]} for res in results]
+                results = cursor.execute(
+                    f"""SELECT item_id, entry FROM FeedItems
+                    WHERE feed_id = %s {last_read_query}""",
+                    values,
+                ).fetchall()
+                return [{"id": res[0], "content": res[1]} for res in results]
 
     def get_all_items(self, username: str, unread_only: bool):
         with self.conn() as conn:
             with conn.cursor() as cursor:
                 user_id = self.get_user_id(cursor, username)
-                unread_query = 'AND items.item_id > feeds.last_read_item_id' if unread_only else ''
-                cursor.execute(f'''
+                unread_query = (
+                    "AND items.item_id > feeds.last_read_item_id" if unread_only else ""
+                )
+                cursor.execute(
+                    f"""
                     SELECT items.entry
                     FROM UserFeeds feeds
                     JOIN FeedItems items ON feeds.feed_id = items.feed_id
                     WHERE feeds.user_id = %s
                     {unread_query}
                     ORDER BY items.published
-                ''', (user_id,))
-                return [{'id': res[0], 'content': res[1]} for res in results]
+                """,
+                    (user_id,),
+                )
+                return [{"id": res[0], "content": res[1]} for res in results]
 
     def mark_as_read(self, username: str, feed_url: str, item_id: int):
         with self.conn() as conn:
             with conn.cursor() as cursor:
                 user_id = self.get_user_id(cursor, username)
                 feed_id = self.get_feed_id(cursor, feed_url)
-                cursor.execute('''UPDATE UserFeeds SET last_read_item_id = %s
-                    WHERE user_id = %s AND feed_id = %s''',
-                    (user_id, feed_id))
-            
+                cursor.execute(
+                    """UPDATE UserFeeds SET last_read_item_id = %s
+                    WHERE user_id = %s AND feed_id = %s""",
+                    (user_id, feed_id),
+                )
+
     def request_feed_update(self, feed_url: str):
         with self.conn() as conn:
             with conn.cursor() as cursor:
-                res = cursor.execute('''UPDATE Feeds SET failed = false
+                res = cursor.execute(
+                    """UPDATE Feeds SET failed = false
                     WHERE feed_url = %s
-                    RETURNING failed''', (feed_url,))
+                    RETURNING failed""",
+                    (feed_url,),
+                )
                 try:
                     was_failed = res.fetchone()[0]
                     return was_failed
                 except:
                     raise FeedNotFound(feed_url)
-                    
 
     def create_feeds_table(self, cursor):
         table = "Feeds"
-        cursor.execute(f'''
+        cursor.execute(
+            f"""
             CREATE TABLE IF NOT EXISTS {table} (
                 feed_id SERIAL PRIMARY KEY,
                 feed_url VARCHAR(255) UNIQUE,
@@ -311,4 +355,5 @@ class DB:
                 modified VARCHAR(255),
                 failed BOOLEAN DEFAULT false
             );
-        ''')
+        """
+        )
--- rss_service/src/updater.py
+++ rss_service/src/updater.py
@@ -11,38 +11,41 @@ import db as db_handler
 UPDATE_INTERVAL_SEC = 2
 MAX_FAIL_COUNT = 3
 
-dramatiq_broker = RabbitmqBroker(parameters=[
-    dict(host='mq'),
-])
+dramatiq_broker = RabbitmqBroker(
+    parameters=[
+        dict(host="mq"),
+    ]
+)
 dramatiq.set_broker(dramatiq_broker)
 
 db = db_handler.DB(
-    os.environ['DBHOST'],
-    os.environ['DBPORT'],
-    os.environ['DBUSER'],
-    os.environ['DBPASSWORD'],
+    os.environ["DBHOST"],
+    os.environ["DBPORT"],
+    os.environ["DBUSER"],
+    os.environ["DBPASSWORD"],
 )
 
-def get_feed_updates(url, etag = None, modified = None):
+
+def get_feed_updates(url, etag=None, modified=None):
     headers = {}
     if etag:
-        headers['If-None-Match'] = etag
+        headers["If-None-Match"] = etag
     if modified:
-        headers['If-Modified-Since'] = modified
+        headers["If-Modified-Since"] = modified
     feed = feedparser.parse(url, etag=etag, modified=modified, request_headers=headers)
     if feed.status == 304:
         entries = []
     else:
         entries = feed.entries
     return {
-        'etag': feed.etag,
-        'modified': feed.modified,
-        'entries': entries,
+        "etag": feed.etag,
+        "modified": feed.modified,
+        "entries": entries,
     }
 
 
 @dramatiq.actor
-def update_feed(url, fail_count = 0):
+def update_feed(url, fail_count=0):
     start_time = time.monotonic()
     if fail_count >= MAX_FAIL_COUNT:
         db.set_failed(url)
@@ -52,23 +55,30 @@ def update_feed(url, fail_count = 0):
         if last_updated is None:
             # The feed does not exist in the table any more
             return
-        updates = get_feed_updates(url, last_updated['etag'], last_updated['modified'])
-        db.put_updates(url, updates['etag'], updates['modified'],
-            [{
-                'published': int(time.mktime(update.published_parsed)),
-                'content': json.dumps(update)
-            } for update in updates['entries']])
+        updates = get_feed_updates(url, last_updated["etag"], last_updated["modified"])
+        db.put_updates(
+            url,
+            updates["etag"],
+            updates["modified"],
+            [
+                {
+                    "published": int(time.mktime(update.published_parsed)),
+                    "content": json.dumps(update),
+                }
+                for update in updates["entries"]
+            ],
+        )
 
         fail_count = 0
     except:
         fail_count += 1
 
-    to_sleep = start_time + UPDATE_INTERVAL_SEC * (10 ** fail_count) - time.monotonic()
+    to_sleep = start_time + UPDATE_INTERVAL_SEC * (10**fail_count) - time.monotonic()
     if to_sleep > 0:
         time.sleep(to_sleep)
     update_feed.send(url, fail_count)
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     for feed in db.list_all_feeds():
         update_feed.send(feed)
